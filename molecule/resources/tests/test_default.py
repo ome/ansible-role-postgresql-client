32f58c839e110952c988a375608b26fa4d6e0a89
Sebastien Besson <seb.besson@gmail.com>
2022-05-19 22:33:07 +0100

    Fix version lookup

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -5,17 +5,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def get_version(hostname):
-    ver_lookup = {
-        "postgresql96": "9.6",
-        "postgresql10": "10",
-        "postgresql11": "11",
-        "postgresql12": "12",
-    }
-    return ver_lookup[hostname]
-
-
 def test_psql_version(host):
-    ver = get_version(host.backend.get_hostname())
+    variables = host.ansible.get_variables()
+    version = variables["postgresql_version"]
     out = host.check_output('psql --version')
-    assert out.startswith('psql (PostgreSQL) {}.'.format(ver))
+    assert out.startswith('psql (PostgreSQL) {}.'.format(version))
